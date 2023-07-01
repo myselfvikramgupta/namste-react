e59bf75a6f7aff248086dfb16538c4a00d5c21c7
vikram_1921 <vikram@binmile.com>
2023-07-01 20:21:22 +0530

    first commit

---
+++ Ep1/App.js
@@ -0,0 +1,3 @@
+// let div=React.createElement('div',{class:"box"},React.createElement("h1",{'id':"heading"},"Hello React"));
+// let root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(div);
